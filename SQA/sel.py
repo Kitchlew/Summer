7bbc246af5d1b77dab288bb0cfc2aaf4a61ed452
Shehreyar Kitchlew <62876048+Kitchlew@users.noreply.github.com>
2022-08-16 21:12:32 +0500

    Update sel.py

--- SQA/sel.py
+++ SQA/sel.py
@@ -1,10 +1,13 @@
 import unittest
-from selenium import webdriver
+from seleniumwire import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
 #This code uses the google chrome browser to conduct a single test on the python.org website
 #It uses the website search bar to search for "pycon"
+# Selenium installation: 3 marks."pip install selenium"
+# Conducting API testing: 5 marks."Created unittest for API Testing"
+# Sharing the right code and configurations used for API testing: 5 marks."Test passed so code is right"
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
@@ -12,14 +15,14 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_search_in_python_org(self):
         driver = self.driver
+        
         driver.get("http://www.python.org")
         self.assertIn("Python", driver.title)
         elem = driver.find_element(By.NAME, "q")
         elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
+        elem.submit()
         self.assertNotIn("No results found.", driver.page_source)
-
-
+      
     def tearDown(self):
         self.driver.close()
 
